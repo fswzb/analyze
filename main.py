ff34371ea6ae7865a010108a9c08c79e5649a6a1
wudan <wudan@shinezone.com>
2016-06-01 22:14:30 +0800

    no message

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ print('hello world')
 
 df = ts.get_hist_data('002337').head(200)
 df['limit_up'] = False
+df['limit_up_r'] = df['close'] * 1.1
 print(df)
 
 indexes = df.index.values
 last_index = None
 for x in indexes:
-    if last_index is not None:
-        
-        pass
-
+    if last_index is None:
+        last_index = x
+        df.loc[x, 'limit_up_r'] = round(df.loc[x, 'open'] * 1.1, 2)
+    else:
+        df.loc[x, 'limit_up_r'] = round(df.loc[last_index, 'close'] * 1.1, 2)
 
 from pylab import *
 
