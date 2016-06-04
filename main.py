fd946a92c27d6743ddab9e7b0e8178a51a939159
marzwu <marzwu@gmail.com>
2016-06-04 08:39:25 +0800

    extract method

--- main.py
+++ main.py
@@ -10,40 +10,43 @@ print(df['p_change'].describe())
 
 from pylab import *
 
-# 按时间顺序展示涨幅
-# n = len(df.index)
-# X = np.arange(n)
-# Y1 = df['p_change'].values
-#
-# bar(X, +Y1, facecolor='#9999ff', edgecolor='white')
-#
-# ylim(-10, +10)
-# show()
+def showTimeLine(df):
+    # 按时间顺序展示涨幅
+    n = len(df.index)
+    X = np.arange(n)
+    Y1 = df['p_change'].values
 
+    bar(X, +Y1, facecolor='#9999ff', edgecolor='white')
 
-# 统计涨幅分布
-X = np.arange(-10, 10, 1)
-Y = list(range(-10, 10, 1))
+    ylim(-10, +10)
+    show()
 
-_index = 0
-for x in range(-10, 10, 1):
-    min = x
-    max = x + 1
-    if x == -10:
-        min = -45
-        max = -9
-    elif x == 9:
-        min = 9
-        max = 45
+def showRateCount(df):
+    # 统计涨幅分布
+    X = np.arange(-10, 10, 1)
+    Y = list(range(-10, 10, 1))
 
-    print('({},{})'.format(min, max))
-    _between = df[min < df['p_change']]
-    _between = _between[_between['p_change'] <= max]
-    Y[_index] = len(_between.index)
+    _index = 0
+    for x in range(-10, 10, 1):
+        min = x
+        max = x + 1
+        if x == -10:
+            min = -45
+            max = -9
+        elif x == 9:
+            min = 9
+            max = 45
 
-    _index += 1
+        print('({},{})'.format(min, max))
+        _between = df[min < df['p_change']]
+        _between = _between[_between['p_change'] <= max]
+        Y[_index] = len(_between.index)
 
-print(Y)
-bar(X, Y, facecolor='#9999ff', edgecolor='white')
+        _index += 1
 
-show()
+    print(Y)
+    bar(X, Y, facecolor='#9999ff', edgecolor='white')
+
+    show()
+
+showRateCount(df)
