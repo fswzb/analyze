939c86cf9d549945459c7805fe4f8fe2ec36e47b
wudan <wudan@shinezone.com>
2017-01-04 22:44:20 +0800

    + 涨停跌停次数

--- growth_limit.py
+++ growth_limit.py
@@ -1,4 +1,5 @@
 # 涨停
+import numpy as np
 import pandas as pd
 
 # hist = pd.read_csv('d:/quant/history/day/data/000001.csv')
@@ -15,11 +16,18 @@ print(hist)
 # 前一天的收盘价
 preclose = list(hist['close'])
 
-preclose.reverse()
-preclose.remove(preclose[0])
-preclose.append(preclose[len(preclose) - 1])
-preclose.reverse()
+preclose.reverse()  # 倒置
+preclose.remove(preclose[0])  # 移除最近一天的
+preclose.append(preclose[len(preclose) - 1])  # 把第一天的收盘价再次插到第0位，第一天的涨幅就算它是0
+preclose.reverse()  # d倒置回来
 
 # 涨幅
 hist['growth'] = (hist['close'] - preclose) / preclose
 print(hist)
+
+hist['limitup'] = np.round(hist['growth'] * 100) / 10
+print(hist)
+
+# 涨停跌停次数
+print((hist['limitup'] == 1).sum())
+print((hist['limitup'] == -1).sum())
