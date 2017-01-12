161d88d8ad05e123cc20105c610c518a8cd2b87d
wudan <wudanhit@163.com>
2017-01-12 16:17:43 +0800

    + 策略

--- new_stock.py
+++ new_stock.py
@@ -11,27 +11,36 @@ def get_new_stock_list():
     开板新股数 167
     上涨数 45
     下跌数 122
-    上涨概率 0.269461077844
-    平均收益率 -0.0461970732001
+    上涨概率: 26.95%
+    平均收益率: -4.62%
+
     第二天的开盘价 - 第一天的开盘价
     开板新股数 167
     上涨数 55
     下跌数 112
-    上涨概率 0.329341317365
-    平均收益率 -0.0293671587794
+    上涨概率: 32.93%
+    平均收益率: -2.94%
+
     第二天的开盘价 - 第一天的最高和最低的中间价
     开板新股数 167
     上涨数 65
     下跌数 102
-    上涨概率 0.389221556886
-    平均收益率 -0.013296470502
+    上涨概率: 38.92%
+    平均收益率: -1.33%
+
     第二天的最高和最低的中间价 - 第一天的最高和最低的中间价
     开板新股数 167
     上涨数 74
     下跌数 93
-    上涨概率 0.443113772455
-    平均收益率 -0.00638910722513
-    :return:
+    上涨概率: 44.31%
+    平均收益率: -0.64%
+
+    第二天的收盘价 - 第一天的收盘价
+    开板新股数 167
+    上涨数 64
+    下跌数 103
+    上涨概率: 38.32%
+    平均收益率: -0.99%
     '''
 
     basics = ts.get_stock_basics()
@@ -41,6 +50,7 @@ def get_new_stock_list():
     strategy2 = []
     strategy3 = []
     strategy4 = []
+    strategy5 = []
     for index in n_stocks.index:
         hist = ts.get_k_data(index)
         hist = hist.tail(len(hist) - 1)
@@ -64,6 +74,8 @@ def get_new_stock_list():
         s_price = (list(hist['high'])[1] + list(hist['low'])[1]) / 2
         strategy4.append((s_price - b_price) / b_price)
 
+        strategy5.append((list(hist['close'])[1] - list(hist['close'])[0]) / list(hist['close'])[0])
+
     print('第二天的收盘价 - 第一天的最高价')
     print_info(np.array(strategy1))
 
@@ -76,13 +88,17 @@ def get_new_stock_list():
     print('第二天的最高和最低的中间价 - 第一天的最高和最低的中间价')
     print_info(np.array(strategy4))
 
+    print('第二天的收盘价 - 第一天的收盘价')
+    print_info(np.array(strategy5))
+
 
 def print_info(n):
     print('开板新股数', len(n))
     print('上涨数', (n > 0).sum())
     print('下跌数', (n <= 0).sum())
-    print('上涨概率', (n > 0).sum() / len(n))
-    print('平均收益率', n.mean())
+    print('上涨概率: {}%'.format(round((n > 0).sum() / len(n) * 100, ndigits=2)))
+    print('平均收益率: {}%'.format(round(n.mean() * 100, ndigits=2)))
+    print()
 
 
 if __name__ == '__main__':
