dd0bea527357a4a442baaf10a94637a8724e1ae4
wudan <wudanhit@163.com>
2017-01-12 15:36:11 +0800

    开板新股数 167 上涨数 45 下跌数 122 上涨概率 0.269461077844 平均收益率 -0.0461970732001

--- new_stock.py
+++ new_stock.py
@@ -1,36 +1,39 @@
 # 新股开板
-# coding=utf8
 import datetime
-import os
-import pandas as pd
+
 import numpy as np
 import tushare as ts
 
 
 def get_new_stock_list():
     basics = ts.get_stock_basics()
-    filter = [row > 20161212 for row in basics['timeToMarket']]
-    n_stocks = basics[filter]
+    _filter = [row > 20160612 for row in basics['timeToMarket']]
+    n_stocks = basics[_filter]
+    diff = []
     for index in n_stocks.index:
         hist = ts.get_k_data(index)
-        hist = hist.tail(len(hist)-1)
-        hist['rise_stop_open'] = hist['high']!=hist['low']
-        filter = []
-        print(hist)
+        hist = hist.tail(len(hist) - 1)
+        hist['rise_stop_open'] = hist['high'] != hist['low']
+        _filter = list(hist['rise_stop_open'])
+        hist = hist[_filter]
 
+        _len = len(hist)
 
-    # lst = os.listdir(u'D:\quant\history\day\data')
-    # for x in lst:
-    #     code = x[0:6]
-    #     hist = pd.read_csv('d:/quant/history/day/data/{}.csv'.format(code))
-    #
-    #     # if hist.tail(1)['date'] > '2015/1/12':
-    #     head = hist.head(1)
-    #     if list(head['date'])[0] < '2015-01-12':
-    #         continue
+        if _len < 2:
+            continue
+
+        # 第二天的收盘价 - 第一天的最高价，算是收益，然后计算收益率
+        diff.append((list(hist['close'])[1] - list(hist['high'])[0]) / list(hist['high'])[0])
+
+    n = np.array(diff)
+    print('开板新股数', len(n))
+    print('上涨数', (n > 0).sum())
+    print('下跌数', (n <= 0).sum())
+    print('上涨概率', (n > 0).sum() / len(n))
+    print('平均收益率', n.mean())
 
 
 if __name__ == '__main__':
     s = datetime.datetime.now()
-    lst = get_new_stock_list()
-    print(datetime.datetime.now()-s)
+    get_new_stock_list()
+    print(datetime.datetime.now() - s)
