ccac2492abd36f7966a7be97a0ad764425844a01
wudan <wudanhit@163.com>
2017-01-12 11:29:15 +0800

    + 新股开板

--- new_stock.py
+++ new_stock.py
@@ -3,18 +3,24 @@
 import os
 import pandas as pd
 import numpy as np
+import tushare as ts
 
 
 def get_new_stock_list():
-    lst = os.listdir(u'D:\quant\history\day\data')
-    for x in lst:
-        code = x[0:6]
-        hist = pd.read_csv('d:/quant/history/day/data/{}.csv'.format(code))
+    basics = ts.get_stock_basics()
+    filter = [row > 20161212 for row in basics['timeToMarket']]
+    print(basics[filter])
 
-        # if hist.tail(1)['date'] > '2015/1/12':
-        head = hist.head(1)
-        if list(head['date'])[0] < '2015-01-12':
-            continue
+
+    # lst = os.listdir(u'D:\quant\history\day\data')
+    # for x in lst:
+    #     code = x[0:6]
+    #     hist = pd.read_csv('d:/quant/history/day/data/{}.csv'.format(code))
+    #
+    #     # if hist.tail(1)['date'] > '2015/1/12':
+    #     head = hist.head(1)
+    #     if list(head['date'])[0] < '2015-01-12':
+    #         continue
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import tushare as ts
 
-print(ts.get_hist_data())
+# print(ts.get_hist_data())
+print(ts.get_suspended())
+print(ts.get_terminated())
