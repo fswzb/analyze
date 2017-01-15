2e8788e3517824299065a41fe59baf87504529d5
wudan <wudan@shinezone.com>
2017-01-15 09:14:46 +0800

    + for each

--- first_negative.py
+++ first_negative.py
@@ -1,6 +1,8 @@
 # 首阴战法
 import datetime
 
+import tushare as ts
+
 
 def is_growth_form(hist):
     '''多头排列达到五天'''
@@ -12,6 +14,18 @@ def is_first_negative(hist):
     pass
 
 
+def is_block_head(hist):
+    '''是否是板块龙头'''
+    pass
+
+
 if __name__ == '__main__':
     s = datetime.datetime.now()
+
+    basics = ts.get_stock_basics()
+    for code in basics.index:
+        hist = ts.get_h_data(code)
+        if not is_growth_form(hist):
+            continue
+
     print(datetime.datetime.now() - s)
