f4262119ae78078f9b3bf4b01490dcd8d56936be
wudan <wudanhit@163.com>
2017-01-16 18:18:11 +0800

    + 二板

--- second_rise_stop.py
+++ second_rise_stop.py
@@ -1,14 +1,21 @@
 import datetime
 import tushare as ts
+from multiprocessing.pool import ThreadPool
 
-def explore_second_rise(hist):
-    return False
+def explore_second_rise(index):
+    hist = ts.get_k_data(index, start='20150116', end='20161216')
+    hist = hist.tail(len(hist) - 30)
+    if len(hist) == 0:
+        return
+    print(len(hist))
 
 if __name__ == '__main__':
     s=datetime.datetime.now()
 
     basics = ts.get_stock_basics()
-    for index, row in basics:
-        continue
+    pool = ThreadPool()
+    pool.map(explore_second_rise, basics.index)
+
+    print('end')
 
     print(datetime.datetime.now()-s)
