9a06e901130f42af8322daff948accc473099ff7
wudan <wudanhit@163.com>
2017-02-07 10:39:02 +0800

    ! 大单

--- sina_dd.py
+++ sina_dd.py
@@ -1,16 +1,24 @@
 """新浪大单"""
+import datetime
 from multiprocessing.pool import ThreadPool
 
 import tushare as ts
 
 
 def get_dd(index):
-    hist = ts.get_sina_dd(index, date='2017-02-06')
+    hist = ts.get_sina_dd(index, date=date,vol=500)
     print(hist)
 
-
+date=None
 if __name__ == '__main__':
+
+    t = datetime.datetime.now().date()
+    date = str(t)
     basics = ts.get_stock_basics()
     tp = ThreadPool()
     # tp.map(get_dd, basics.index)
-    tp.map(get_dd, ['300268'])
+    # tp.map(get_dd, ['300268'])
+    # tp.map(get_dd, ['002591'])
+    # tp.map(get_dd, ['300323'])
+    # tp.map(get_dd, ['002451'])
+    tp.map(get_dd, ['002552'])
