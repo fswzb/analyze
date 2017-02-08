ed705cd48bbe32421f66f6299e296a73a60c68af
wudan <wudanhit@163.com>
2017-02-08 10:46:23 +0800

    + 增加失败重试机制

--- history.py
+++ history.py
@@ -1,60 +1,76 @@
 import datetime
 import os
+import threading
 from multiprocessing.pool import ThreadPool
+from time import sleep
 
-import redis
 import tushare as ts
 
 
 def update(index):
     print('update', index)
 
-    # hist = ts.get_h_data(index)
-    hist = ts.get_hist_data(index)
+    try:
+        # hist = ts.get_h_data(index)
+        hist = ts.get_hist_data(index)
 
-    root_path = 'd:/analyze_data'
-    if not os.path.exists(root_path):
-        os.makedirs(root_path)
+        root_path = 'd:/analyze_data'
+        if not os.path.exists(root_path):
+            os.makedirs(root_path)
 
-    k_path = '{}/k'.format(root_path)
-    if not os.path.exists(k_path):
-        os.makedirs(k_path)
+        k_path = '{}/k'.format(root_path)
+        if not os.path.exists(k_path):
+            os.makedirs(k_path)
 
-    hist.to_csv('{}/{}.csv'.format(k_path, index))
+        hist.to_csv('{}/{}.csv'.format(k_path, index))
 
-    # r = redis.Redis(connection_pool=redis_pool)
+        # r = redis.Redis(connection_pool=redis_pool)
 
-    tick_path = '{}/tick/{}'.format(root_path, index)
-    if not os.path.exists(tick_path):
-        os.makedirs(tick_path)
+        tick_path = '{}/tick/{}'.format(root_path, index)
+        if not os.path.exists(tick_path):
+            os.makedirs(tick_path)
 
-    for i, row in hist.iterrows():
-        date = str(i)
-        # key = '{}_{}'.format(index, date)
+        for i, row in hist.iterrows():
+            date = str(i)
+            # key = '{}_{}'.format(index, date)
 
-        # if r.exists(key):
-        #     continue
+            # if r.exists(key):
+            #     continue
 
-        # print(key)
+            # print(key)
 
-        filename = '{}/{}.csv'.format(tick_path, date)
-        if os.path.exists(filename):
-            continue
+            filename = '{}/{}.csv'.format(tick_path, date)
+            if os.path.exists(filename):
+                continue
 
-        print(filename)
-        tick = ts.get_tick_data(index, date=date)
-        # r.set(key, tick.to_string())
-        tick.to_csv(filename)
+            print(filename)
+            tick = ts.get_tick_data(index, date=date)
+            # r.set(key, tick.to_string())
+            tick.to_csv(filename)
+    except:
+        mu.acquire()
+        fail_pool.append(index)
+        mu.release()
+
+        print('{} fail, put into fail pool'.format(index))
 
 
-redis_pool = None
+# redis_pool = None
+fail_pool = []
+mu = threading.Lock()
 if __name__ == '__main__':
     t = datetime.datetime.now()
 
-    redis_pool = redis.ConnectionPool(host='127.0.0.1', port='6379')
+    # redis_pool = redis.ConnectionPool(host='127.0.0.1', port='6379')
 
     basics = ts.get_stock_basics()
+    index_pool = basics.index
+
     tp = ThreadPool()
-    tp.map(update, basics.index)
+    while len(index_pool) > 0:
+        tp.map(update, index_pool)
+        index_pool = fail_pool
+        fail_pool = []
+        sleep(5)
 
     print(datetime.datetime.now() - t)
--- test.py
+++ test.py
@@ -1,5 +1,17 @@
-import tushare as ts
+from multiprocessing.pool import ThreadPool
+from random import random
+
 
 # print(ts.get_hist_data())
-print(ts.get_suspended())
-print(ts.get_terminated())
+# print(ts.get_suspended())
+# print(ts.get_terminated())
+
+def get_random(code):
+    if code < .5:
+        return code < .5
+
+
+if __name__ == '__main__':
+    tp = ThreadPool()
+    r = tp.map(get_random, [random(), random(), random(), random()])
+    print(r)
