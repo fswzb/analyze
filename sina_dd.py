101f2eb94eef9409ed96b4472de0737bc7e9f18a
wudan <wudanhit@163.com>
2017-02-09 17:52:15 +0800

    ! 重构

--- sina_dd.py
+++ sina_dd.py
@@ -7,8 +7,13 @@ import tushare as ts
 
 def get_dd(index):
     hist = ts.get_sina_dd(index, date=date, vol=500)
-    # print(hist)
+    print(hist)
     print(hist['volume'].sum())
+    buy = hist[hist['type'].str.contains('买盘')]['volume'].sum()
+    sell = hist[hist['type'].str.contains('卖盘')]['volume'].sum()
+    print('买盘', buy)
+    print('卖盘', sell)
+    print(round(buy / sell, 2))
 
 
 date = None
@@ -20,10 +25,6 @@ if __name__ == '__main__':
     # basics = ts.get_stock_basics()
     tp = ThreadPool()
     # tp.map(get_dd, basics.index)
-    # tp.map(get_dd, ['300268'])
-    # tp.map(get_dd, ['002591'])
-    # tp.map(get_dd, ['300323'])
-    tp.map(get_dd, ['601177'])
-    # tp.map(get_dd, ['002552'])
+    tp.map(get_dd, ['002825'])
 
     print(datetime.datetime.now() - t)
