daf84c2e28e1ccb9f2c0b9a661c1bbdaf4767d86
wudan <wudan@shinezone.com>
2017-02-09 22:42:08 +0800

    + 扫描大单

--- scan_big_bid.py
+++ scan_big_bid.py
@@ -1,3 +1,34 @@
 """大单扫货到，使股价达到涨停"""
+import datetime
+from multiprocessing.pool import ThreadPool
+
+import pandas as pd
+import tushare as ts
+
+
+def get_bid(index):
+    hist = ts.get_sina_dd(index, date=date, vol=500)
+    if hist is not None:
+        print(index, hist['volume'].sum())
+
+
+date = None
 if __name__ == '__main__':
-    pass
+    t = datetime.datetime.now()
+
+    date = t.date()
+    date = str(date)
+
+    try:
+        basics = ts.get_stock_basics()
+        basics.to_csv('d:/analyze_data/all.csv')
+    except:
+        basics = pd.read_csv('d:/analyze_data/all.csv')
+
+    basics = basics[basics['outstanding'] < 5]
+    print(len(basics))
+
+    tp = ThreadPool()
+    tp.map(get_bid, basics.index)
+
+    print(datetime.datetime.now() - t)
