ef5d1f61a48ab884b624452f08838c3cf2de1c7e
wudan <wudan@shinezone.com>
2017-02-11 17:05:16 +0800

    + 更新大单数据

--- history.py
+++ history.py
@@ -9,7 +9,7 @@ import tushare as ts
 from pandas.compat import StringIO
 
 
-def update(index):
+def update_tick_and_dd(index):
     print('update', index)
 
     try:
@@ -34,6 +34,10 @@ def update(index):
         if not os.path.exists(tick_path):
             os.makedirs(tick_path)
 
+        dd_path = '{}/dd/{}'.format(root_path, index)
+        if not os.path.exists(dd_path):
+            os.makedirs(dd_path)
+
         for i, row in hist.iterrows():
             date = str(i)
             # key = '{}_{}'.format(index, date)
@@ -43,19 +47,26 @@ def update(index):
 
             # print(key)
 
+            # tick数据
             filename = '{}/{}.csv'.format(tick_path, date)
-            if os.path.exists(filename):
-                continue
+            if not os.path.exists(filename):
+                print(filename)
+                tick = ts.get_tick_data(index, date=date)
+                tick.to_csv(filename)
 
-            print(filename)
-            tick = ts.get_tick_data(index, date=date)
-            # r.set(key, tick.to_string())
-            tick.to_csv(filename)
-    except:
+            # 大单数据
+            filename = '{}/{}.csv'.format(dd_path, date)
+            if not os.path.exists(filename):
+                print(filename)
+                dd = ts.get_sina_dd(index, date=date, vol=500)
+                if dd is not None:
+                    dd.to_csv(filename)
+    except Exception as e:
         mu.acquire()
         fail_pool.append(index)
         mu.release()
 
+        print(e)
         print('{} fail, put into fail pool'.format(index))
 
 
@@ -80,7 +91,7 @@ if __name__ == '__main__':
 
     tp = ThreadPool()
     while len(index_pool) > 0:
-        tp.map(update, index_pool)
+        tp.map(update_tick_and_dd, index_pool)
         index_pool = fail_pool
         fail_pool = []
         sleep(5)
