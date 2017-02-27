eaa4943c48a6d724e53e6ec7037b842bc5f3a6da
wudan <wudan@shinezone.com>
2017-02-28 00:03:53 +0800

    + bbi-量比-换手率 策略

---
+++ bbi_strategy.py
@@ -0,0 +1,37 @@
+"""BBI大于5小于10；量比大于0.5小于3；换手率大于2%小于7%；按总市值从小到大排列"""
+import os
+from io import StringIO
+
+import pandas as pd
+import tushare as ts
+
+if __name__ == '__main__':
+    # basics = get_stock_basics()
+    basics = ts.get_stock_basics()
+    hist = ts.get_hist_data('sh')
+    poll = pd.DataFrame(columns=['code', 'bbi', '量比', 'turnover', 'totalAssets'])
+    date = hist.index[0]
+    for index, row in basics.iterrows():
+        filename = 'd:/analyze_data/k/{}.csv'.format(index)
+        if os.path.exists(filename):
+            text = open(filename, encoding='GBK').read()
+            text = text.replace('--', '')
+            df = pd.read_csv(StringIO(text), dtype={'date': 'object'})
+            df = df.set_index('date')
+
+            if df.index[0] == date:
+                row['bbi'] = (df['close'].head(3).mean() + df['close'].head(6).mean() + df['close'].head(12).mean() +
+                              df['close'].head(24).mean()) / 4
+                row['量比'] = df['volume'][0] / (df['volume'].head(6).tail(5).mean())
+                row['turnover'] = df['turnover'][0]
+                poll = poll.append({'code': index, 'bbi': row['bbi'], '量比': row['量比'], 'turnover': row['turnover'],
+                                    'totalAssets': row['totals'] * df['close'][0]}, ignore_index=True)
+
+    # print(poll)
+
+    poll = poll[poll['turnover'].between(2, 7)]
+    poll = poll[poll['量比'].between(0.5, 3)]
+    poll = poll[poll['bbi'].between(5, 10)]
+    poll = poll.sort_values('totalAssets')
+
+    print(poll)
