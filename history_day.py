1a599aba45386396a6cc6b130dccfcea52fb8ead
wudan <wudan@shinezone.com>
2017-03-03 23:04:17 +0800

    + try

--- history_day.py
+++ history_day.py
@@ -11,35 +11,39 @@ from utils import get_stock_basics
 
 def update_day():
     def f(iterrow):
-        code = iterrow[0]
-        info = iterrow[1]
+        try:
+            code = iterrow[0]
+            info = iterrow[1]
 
-        print(code)
+            print(code)
 
-        filename = 'd:/analyze_data/k/{}.csv'.format(code)
-        if os.path.exists(filename):
-            text = open(filename, encoding='GBK').read()
-            text = text.replace('--', '')
-            hist = pd.read_csv(StringIO(text), dtype={'date': 'object'})
-            hist = hist.set_index('date')
+            filename = 'd:/analyze_data/k/{}.csv'.format(code)
+            if os.path.exists(filename):
+                text = open(filename, encoding='GBK').read()
+                text = text.replace('--', '')
+                hist = pd.read_csv(StringIO(text), dtype={'date': 'object'})
+                hist = hist.set_index('date')
 
-            hist_close = hist['close']
-            for i in range(len(hist)):
-                k = hist.iloc[i]
-                date = k.name
+                hist_close = hist['close']
+                for i in range(len(hist)):
+                    k = hist.iloc[i]
+                    date = k.name
 
-                d = {'code': code, 'date': date, 'outstanding': info['outstanding'], 'totals': info['totals'],
-                     'totalAssets': info['totals'] * k['close']}
-                d['bbi'] = (hist_close.iloc[i:i + 3].mean() + hist_close.iloc[i:i + 6].mean()
-                            + hist_close.iloc[i:i + 12].mean() + hist_close.iloc[i:i + 24].mean()) / 4
-                d['量比'] = k['volume'] / (hist['volume'].iloc[i:i + 6].tail(5).mean())
-                d['turnover'] = k['turnover']
+                    d = {'code': code, 'date': date, 'outstanding': info['outstanding'], 'totals': info['totals'],
+                         'totalAssets': info['totals'] * k['close']}
+                    d['bbi'] = (hist_close.iloc[i:i + 3].mean() + hist_close.iloc[i:i + 6].mean()
+                                + hist_close.iloc[i:i + 12].mean() + hist_close.iloc[i:i + 24].mean()) / 4
+                    d['量比'] = k['volume'] / (hist['volume'].iloc[i:i + 6].tail(5).mean())
+                    d['turnover'] = k['turnover']
 
-                mu.acquire()
-                df = all_days[date] if date in all_days else pd.DataFrame(columns=columns)
-                df = df.append(d, ignore_index=True)
-                all_days[date] = df
-                mu.release()
+                    mu.acquire()
+                    df = all_days[date] if date in all_days else pd.DataFrame(columns=columns)
+                    df = df.append(d, ignore_index=True)
+                    all_days[date] = df
+                    mu.release()
+        except Exception as e:
+            print(e)
+            print(iterrow)
 
     all_days = {}
     columns = ['code', 'date', 'outstanding', 'totals', 'totalAssets', 'bbi', '量比', 'turnover']
